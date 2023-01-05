feffd8905a826fa199dfcea172338d63e6e710eb
Great0106 <raufderiel@gmail.com>
2023-01-05 15:14:59 +0700

    django html and css

--- mywebsite/todo/views.py
+++ mywebsite/todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Ini adalah halaman utama todo")
+    return render(request, 'todo/index.html')
 
-def title(request):
-    return HttpResponse("<h1>ini adalah halaman title</h1>")
+
+
